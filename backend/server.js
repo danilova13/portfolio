78080bfeca7b4355d871b060f6e768f5ddddafb2
Anna Danilova <anyadanilova13@gmail.com>
2023-04-05 11:10:31 -0400

    intialized server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// import express from "express";
+
+// const app = express();
+
+// app.get('/', (req,res  => {
+// 	res.send("Server is Ready");
+// }))
+
+// app.listen(1000, () => {
+// 	console.log("Server at http://localhost:1000");
+// })
+
+console.log('Hello World'); 
