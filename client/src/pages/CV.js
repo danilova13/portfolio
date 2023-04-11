93e8d26a4ae1d4dcca6da556a58b9d894a9deec9
Anna Danilova <anyadanilova13@gmail.com>
2023-04-11 17:21:31 -0400

    implemented 2 ways to download cv

--- client/src/pages/CV.js
+++ client/src/pages/CV.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import resume from '../images/CV_AnnaDanilova.pdf';
+import fileSaver from "file-saver";
+
 
 const CV = () => {
 
@@ -13,11 +16,33 @@ const CV = () => {
 			.then(data => setCV(prev => data))
 			.catch(err => console.log(err))
 	}, [])
+
+	const download =(e) => {
+		fetch(`http://localhost:8000`, {
+			method: "GET",
+			responseType: "blob"
+		})
+		.then((res) => {
+			//check if response is valid
+			if (!res.ok){
+				throw new Error(`HTTP error! status: ${res.status}`)
+			}
+			return res.blob();
+		})
+		.then((blob) => {
+			//use file-saver library to save the file
+			fileSaver.saveAs(blob, "resume.pdf");
+		})
+		.catch((error) => {
+			console.log("Error fetching file:", error);
+		})
+
+	}
 	  
 	return ( 
 		<div className="snap-start w-screen h-screen items-center justify-left bg-gradient-to-br from-orange-200 to-gray-200" >
 
-			<div className="grid grid-cols-1 pt-20">
+			<div className="grid grid-cols-1 pt-20 pl-10">
 				<div className="grid grid-cols-1">
 					<div className="flex items-center justify-left pl-10 max-h-14">
 						<h1 className="text-gray-700 font-semibold text-3xl mr-5">Education</h1>
@@ -61,6 +86,25 @@ const CV = () => {
 				</div>
 
 			</div>
+
+			{/* <div className="pl-20 pt-10"> 
+				<a href={resume} download="Resume"> 
+					<button className="inline-block bg-gray-600 rounded-full px-3 py-1 text-sm font-semibold text-white ml-2">
+						Download Resume 
+					</button>
+				</a>
+			</div> */}
+
+			<div className="pl-20 pt-10"> 
+					<button 
+						onClick={(e) => download(e)}
+						className="inline-block bg-pink-800 rounded-full px-3 py-1 text-sm font-semibold text-white ml-2">
+						Download Resume 
+					</button>
+			</div>
+			
+			
+			
 		</div>
 	 );
 }
